529c7ec225af201c0b2feef0a4aee1224e6ec458
User Freepk <freepk@azooma.ru>
2015-03-09 21:25:40 +0000

    .

--- node.go
+++ node.go
@@ -9,12 +9,12 @@ type color uint8
 
 type node struct {
 	x int
-	r color
+	c color
 	l [2]*node
 }
 
-func newNode(x int, r color) *node {
-	return &node{x: x, r: r, l: [2]*node{nil, nil}}
+func newNode(x int, c color) *node {
+	return &node{x: x, c: c, l: [2]*node{nil, nil}}
 }
 
 func (n *node) rotate(d direction) *node {
@@ -33,14 +33,14 @@ func (n *node) insert(x int) (*node, bool) {
 	}
 	p, pd := s.pop()
 	p.l[pd] = newNode(x, red)
-	for p.r == red {
+	for p.c == red {
 		g, gd := s.pop()
 		ud := ^gd & 1
-		if u := g.l[ud]; u != nil && u.r == red {
-			u.r = black
-			p.r = black
+		if u := g.l[ud]; u != nil && u.c == red {
+			u.c = black
+			p.c = black
 			if g != n {
-				g.r = red
+				g.c = red
 				p, pd = s.pop()
 				continue
 			}
@@ -49,8 +49,8 @@ func (n *node) insert(x int) (*node, bool) {
 			p = p.rotate(pd)
 			g.l[gd] = p
 		}
-		g.r = red
-		p.r = black
+		g.c = red
+		p.c = black
 		switch {
 		case g == n:
 			return g.rotate(gd), true
