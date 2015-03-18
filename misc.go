e23be7f32bafba16fa15e30739d6a3e3b4e15553
freepk <freepk@mail.ru>
2015-03-18 11:52:19 +0000

    .

--- misc.go
+++ misc.go
@@ -13,3 +13,38 @@ func traverse(n *node) {
 		traverse(n.l[right])
 	}
 }
+
+func isRed(n *node) bool {
+	return (n != nil) && (n.c == red)
+}
+
+func isValid(n *node) int {
+	if n == nil {
+		return 1
+	} else {
+		ln := n.l[left]
+		rn := n.l[right]
+		if isRed(n) {
+			if isRed(ln) || isRed(rn) {
+				return 0
+			}
+		}
+		lh := isValid(ln)
+		rh := isValid(rn)
+		if (ln != nil && ln.x >= n.x) || (rn != nil && rn.x <= n.x) {
+			return 0
+		}
+		if (lh != 0) && (rh != 0) && (lh != rh) {
+			return 0
+		}
+		if (lh != 0) && (rh != 0) {
+			if isRed(n) {
+				return lh
+			} else {
+				return lh + 1
+			}
+		} else {
+			return 0
+		}
+	}
+}
--- node_test.go
+++ node_test.go
@@ -12,18 +12,40 @@ func TestInsert(t *testing.T) {
 	}
 	n, _ = n.insert(0)
 	n, _ = n.insert(0)
-}
-
-func BenchmarkInsSeq(b *testing.B) {
-	n := newNode(0, black)
-	for i := 0; i < b.N; i++ {
-		n, _ = n.insert(i)
+	if isValid(n) == 0 {
+		t.Fail()
 	}
 }
 
-func BenchmarkInsRnd(b *testing.B) {
+func insertSeq(c int) *node {
 	n := newNode(0, black)
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < c; i++ {
+		n, _ = n.insert(i)
+	}
+	return n
+}
+
+func insertRnd(c int) *node {
+	n := newNode(0, black)
+	for i := 0; i < c; i++ {
 		n, _ = n.insert(rand.Int())
+	}
+	return n
+}
+
+func BenchmarkInsSeq(b *testing.B) {
+	insertSeq(b.N)
+}
+
+func BenchmarkInsRnd(b *testing.B) {
+	insertRnd(b.N)
+}
+
+func BenchmarkSearchSeq(b *testing.B) {
+	b.N = 1000000
+	n := insertSeq(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		n.search(i)
 	}
 }
