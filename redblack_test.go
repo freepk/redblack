1809a1bb42082bcd01621075856f4cd1418a1058
freepk <freepk@mail.ru>
2018-03-29 14:44:04 +0300

    Update redblack_test.go

--- redblack_test.go
+++ redblack_test.go
@@ -21,6 +21,32 @@ func benchmarkTreeRnd(n int) *Tree {
 	return t
 }
 
+func TestNewStack(t *testing.T) {
+	s := stack{}
+	s.push(newNode(10, red), left)
+	s.push(newNode(20, black), right)
+	n, d := s.pop()
+	if n.k != 20 || n.c != black || d != right {
+		t.Fail()
+	}
+	n, d = s.pop()
+	if n.k != 10 || n.c != red || d != left {
+		t.Fail()
+	}
+}
+
+func BenchmarkStackPushAndPop(b *testing.B) {
+	s := stack{}
+	for i := 0; i < b.N; i++ {
+		switch (i / stackSize) % 2 {
+		case 0:
+			s.push(nil, left)
+		case 1:
+			s.pop()
+		}
+	}
+}
+
 func TestTreeCommon(t *testing.T) {
 	var tr *Tree
 	tr = benchmarkTreeSeq(10000)
